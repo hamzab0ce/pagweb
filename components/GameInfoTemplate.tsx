c8a8fdd2e9cb8368eba6bd7ec043ff2ad554acb6
danielquirozc <142172231+danielquirozc@users.noreply.github.com>
2025-05-11 18:37:13 -0400

    Reemplaza componentes de imagen de Next.js por etiquetas img en GameInfoTemplate y GoalCard

--- components/GameInfoTemplate.tsx
+++ components/GameInfoTemplate.tsx
@@ -2,30 +2,27 @@ import type { GamesWithLinks } from "@/app/actions/getGames";
 import { useModalStore } from "@/stores/modalStore";
 import DownloadLinkList from "./DownLoadLinkList";
 import GenreBadge from "./GenreBadge";
-import Image from "next/image";
 
 export default function GameTemplate({ game }: { game: GamesWithLinks }) {
   const modalStore = useModalStore();
   return (
     <>
-      <div className="flex items-start justify-between p-5 border-b border-solid border-zinc-800 rounded-t">
+      <div className="flex items-center justify-between p-5 border-b border-solid border-zinc-800 rounded-t">
         <h3 className="text-3xl w-full font-semibold text-zinc-100">
           {game.title}
         </h3>
         <button
-          className="p-1 ml-auto cursor-pointer bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+          className="p-1 ml-auto cursor-pointer bg-transparent border-0 text-zinc-300 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
           onClick={() => modalStore.closeModal()}
         >
-          <span className="bg-transparent text-zinc-100 h-6 w-6 text-2xl block outline-none focus:outline-none">
-            ×
-          </span>
+          ×
         </button>
       </div>
       <div className="relative p-5 flex-auto">
         <GenreBadge genres={game.games_genres} />
         <div className="flex gap-5">
           <div className="flex flex-col h-72 min-w-58">
-            <Image
+            <img
               src={game.cover_url}
               alt={game.title}
               className="object-cover h-full w-full  rounded-xl"
--- components/Home/GoalCard.tsx
+++ components/Home/GoalCard.tsx
@@ -1,6 +1,5 @@
 import { useModalStore } from "@/stores/modalStore";
 import type { GamesWithLinks } from "@/app/actions/getGames";
-import Image from "next/image";
 
 export default function GoalCard({game
 }: {
@@ -13,7 +12,7 @@ export default function GoalCard({game
   return (
     <li onClick={onClick} className="max-w-2xs cursor-pointer relative rounded-md overflow-hidden">
       <div className="flex flex-col items-center justify-center rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out">
-        <Image src={game.cover_url} alt={game.title} className="object-cover h-72 w-56" />
+        <img src={game.cover_url} alt={game.title} className="object-cover h-72 w-56" />
         <h2 className="mt-4 absolute bottom-0 bg-linear-to-b from-transparent to-80% to-black w-full p-2 text-md text-white">
           {game.title}
         </h2>
