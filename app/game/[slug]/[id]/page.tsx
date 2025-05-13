062ec6123362387f4e0284e7f879ef0f98f53ee4
danielquirozc <142172231+danielquirozc@users.noreply.github.com>
2025-05-13 02:38:04 -0400

    Añade generación de metadatos para la página del juego y actualiza la metadata global con información de Open Graph.

--- app/game/[slug]/[id]/page.tsx
+++ app/game/[slug]/[id]/page.tsx
@@ -1,6 +1,26 @@
 import { getGameByID } from "@/app/actions/getGameByID";
 import GameTemplate from "@/components/GameInfoTemplate";
+import { Metadata } from "next";
 import Link from "next/link";
+
+export async function generateMetadata({ params } : {
+  params: Promise<{ id: number }>
+}) : Promise<Metadata> {
+  const { id } = await params;
+  const game = await getGameByID(Number(id));
+  return {
+    title: game?.title || "Juego no encontrado",
+    icons: game?.cover_url || "/favicon.ico",
+    openGraph: {
+      title: game?.title || "Juego no encontrado",
+      description: "Pagina oficial de Wave Repacks, una coleccion de repacks de juegos para PC y consolas.",
+      siteName: "Wave Repacks",
+      locale: "es-ES",
+      type: "website",    
+      images: game?.cover_url || "/favicon.ico",
+    }
+  };
+}
 
 export default async function Page({
   params,
