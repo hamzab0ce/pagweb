01afc5db5a73ecec7bb0202fce1232c0f03e3730
hamzab0ce <hamzaboce2001@gmail.com>
2025-07-03 12:36:46 +0200

    Update GoalWrapper.tsx

--- components/Home/GoalWrapper.tsx
+++ components/Home/GoalWrapper.tsx
@@ -1,17 +1,16 @@
 import { getGames } from "@/app/actions/getGames";
 import GoalList from "./GoalList";
 import { countGames } from "@/app/actions/countGame";
-
-export default async function GoalWrapper({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
+export default async function GoalWrapper({ searchParams }: {
+  searchParams: { [key: string]: string | string[] | undefined }
 }) {
-  const skip = Number(searchParams.page) || 0;
-  const category = (searchParams.category as string) || "";
-
+  // No necesitas esperar a searchParams, ya que es un objeto
+  const skip = Number(searchParams.page) || 0; 
+  const category = searchParams.category as string || "";
   const games = await getGames(skip, category);
   const count = await countGames();
-
-  return <GoalList games={games} totalGames={count} />;
+  
+  return (
+    <GoalList games={games} totalGames={count} />
+  );
 }
